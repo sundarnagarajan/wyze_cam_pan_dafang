5bf8d495044236841a37f9643c7149115862151b
Sundar Nagarajan <sun.nagarajan@gmail.com>
2019-11-20 20:19:32 -0800

    Added Openfang kernel, kernel config, Dafang Hacks toolchain dded compilation defs.sh and buildall.sh

---
+++ compile/buildall.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+if [ -n "$BASH_SOURCE" ]; then
+    PROG_PATH=${PROG_PATH:-$(readlink -e $BASH_SOURCE)}
+else
+    PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
+fi
+PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
+PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
+source ${PROG_DIR}/defs/sh || exit 1
+
+SECONDS=0
+
+cd ${KERNEL_DIR}
+$MAKE_THREADED clean
+\cp -f ${DAFANG_DIR}/.config .
+( $MAKE_THREADED oldconfig && $MAKE_THREADED uImage && $MAKE_THREADED modules && $MAKE_THREADED ) || exit 1
+
+cd ${DRIVERS_DIR}
+for d in $(find -type d)
+do
+    if [ -f "${d}/Makefile" ]; then
+        echo "Executing make clean in $d"
+        cd $d
+        $MAKE_THREADED clean 1>/dev/null 2>&1
+        cd - 1>/dev/null
+    fi
+done
+find . -name '*.ko' -exec rm -f {} \;
+for d in audio/alsa isp/tx-isp misc/sample_motor misc/sensor_info sensors/jxf22 sensors/jxh62;
+do
+    cd $d
+    $MAKE_THREADED
+    cd - 1>/dev/null
+done
+
+# Now assemble all built modules and generate modules.dep
+# Note: depmod ASSUMES kernel version == running (HOST) kernel version
+rm -rf ${DAFANG_NEW_DIR}
+
+mkdir -p ${DEPMOD_MODULES_DIR}
+
+find ${KERNEL_DIR} -name '*.ko' -exec cp {} ${DEPMOD_MODULES_DIR}/ \;
+find ${DRIVERS_DIR} -name '*.ko' -exec cp {} ${DEPMOD_MODULES_DIR}/ \;
+for f in modules.builtin modules.order
+do
+    cp ${KERNEL_DIR}/$f ${DEPMOD_MODULES_DIR}/
+done
+
+depmod -b ${DAFANG_NEW_DIR}/depmod -a
+
+mkdir -p ${BUILT_MODULES_DIR}
+cp -v ${DEPMOD_MODULES_DIR}/* ${BUILT_MODULES_DIR}/
+rm -rf ${DEPMOD_DIR}
+
+# Copy kernel
+mkdir -p ${BUILT_KERNEL_DIR}
+find ${KERNEL_DIR} -name 'uImage.lzma' -exec cp {} ${BUILT_KERNEL_DIR}/ \;
+cp ${BUILT_KERNEL_DIR}/uImage.lzma ${BUILT_KERNEL_DIR}/${NEW_KERNEL_FILENAME}
+mkimage -l ${BUILT_KERNEL_DIR}/${NEW_KERNEL_FILENAME}
+
+echo "SECONDS = $SECONDS"
---
+++ compile/defs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ -n "$BASH_SOURCE" ]; then
+    PROG_PATH=${PROG_PATH:-$(readlink -e $BASH_SOURCE)}
+else
+    PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
+fi
+PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
+PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
+
+DAFANG_DIR=$(readlink -e ${PROG_DIR}/..)
+DAFANG_TOOLCHAIN_DIR=${DAFANG_DIR}/toolchain
+KERNEL_DIR=${DAFANG_DIR}/kernel-3.10.14
+DRIVERS_DIR=${DAFANG_DIR}/drivers
+DAFANG_NEW_DIR=${DAFANG_DIR}/new
+DEPMOD_DIR=${DAFANG_NEW_DIR}/depmod
+DEPMOD_MODULES_DIR=${DEPMOD_DIR}/lib/modules/$(uname -r)
+BUILT_MODULES_DIR=${DAFANG_NEW_DIR}/modules
+BUILT_KERNEL_DIR=${DAFANG_NEW_DIR}/kernel
+NEW_KERNEL_FILENAME=kernel-t20.bin
+
+export ARCH=mips
+export CROSS_COMPILE=${DAFANG_TOOLCHAIN_DIR}/bin/mips-linux-gnu-
+export CC=${CROSS_COMPILE}gcc
+export KSRC=$KERNEL_DIR
+MAKE_THREADED='make -j$(nproc)'
+
+
+for v in DAFANG_DIR DAFANG_NEW_DIR DAFANG_TOOLCHAIN_DIR KERNEL_DIR DRIVERS_DIR BUILT_MODULES_DIR DEPMOD_DIR DEPMOD_MODULES_DIR BUILT_KERNEL_DIR NEW_KERNEL_FILENAME
+do
+    printf '%-32s : %s\n' "$v" "${!v}"
+done
+ls -l $CC
