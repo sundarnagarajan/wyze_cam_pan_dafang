21d549837c7855b5520dacdadb03b448563960b9
Sundar Nagarajan <sun.nagarajan@gmail.com>
2019-11-21 19:38:48 -0800

    tee to LOG_FILE

--- compile/buildall.sh
+++ compile/buildall.sh
@@ -12,14 +12,14 @@ SECONDS=0
 rm -f $LOG_FILE
 
 echo "Cleaning kernel"
-clean_kernel 2>&1 | show_dots_per_file
+clean_kernel 2>/dev/null | show_dots_per_file
 echo "Cleaning drivers"
 clean_drivers
-patch_kernel 2>&1 | show_dots_per_file
+patch_kernel 2>/dev/null | show_dots_per_file
 echo "Building kernel"
-build_kernel 2>&1 | show_dots_per_file
+build_kernel 2>/dev/null | show_dots_per_file
 echo "Building drivers"
-build_drivers 2>&1 | show_dots_per_file
+build_drivers 2>/dev/null | show_dots_per_file
 run_depmod
 copy_built_files
 echo ""
